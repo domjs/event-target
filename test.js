0edf7d87128addebc726f25a3008681fe6c0f27b
Nathan Rajlich <nathan@tootallnate.net>
2013-08-21 16:36:07 -0700

    test: add some stuffs

--- test.js
+++ test.js
@@ -2,16 +2,41 @@
 var EventTarget = require('./');
 console.log(EventTarget.prototype);
 
-var e = new EventTarget();
-console.log(e);
+var target = new EventTarget();
+console.log(target);
 
-e.addEventListener('test', function (e) {
+function first (e) {
   console.error('"test" event', e);
-});
-console.log(e);
+  throw new Error('test error');
+}
+target.addEventListener('test', first, false);
+console.log(target);
 
 var event = {
   type: 'test'
 };
-var rtn = e.dispatchEvent(event);
+var rtn = target.dispatchEvent(event);
 console.log('dispatchEvent rtn:', rtn);
+
+
+
+function second (e) {
+  console.error('"test" event 2', e);
+  e.defaultPrevented = true;
+  throw new Error('test error 2');
+}
+target.addEventListener('test', second, false);
+console.log(target);
+
+rtn = target.dispatchEvent(event);
+console.log('dispatchEvent rtn 2:', rtn);
+
+target.removeEventListener('test', second);
+
+rtn = target.dispatchEvent(event);
+console.log('dispatchEvent rtn 3:', rtn);
+
+target.removeEventListener('test', first);
+
+rtn = target.dispatchEvent(event);
+console.log('dispatchEvent rtn 4:', rtn);
